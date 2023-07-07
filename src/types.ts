dfc464b78f203c088ddf9a5ae4bfdc9d7d56afeb
fredrik9X6 <fredrik@9x6.se>
2023-07-07 14:17:37 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,8 @@
 export type Event = {
-    title: string;
-    start: string | Date;
-    end: string | Date;
-    lodge: string;
-    stuga: string;
-    stugaid: string;
-  };
-  
+  title: string;
+  start: string | Date;
+  end: string | Date;
+  lodge: string;
+  stuga: string;
+  stugaid: string;
+};
